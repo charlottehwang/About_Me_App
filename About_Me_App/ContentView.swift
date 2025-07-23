30322fda7ea569f59e75a5bd479155456c0e0352
Charlotte <charlotteehwang@gmail.com>
2025-07-23 13:22:29 -0700

    VStack, HStack, font and colors

--- About_Me_App/ContentView.swift
+++ About_Me_App/ContentView.swift
@@ -9,23 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        
+        VStack(spacing: 20.0) {
             Text("ALL ABOUT ME!!")
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .foregroundColor(Color.pink)
-            
+            Text("Charlotte Hwang")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .foregroundColor(Color(hue: 0.88, saturation: 0.939, brightness: 0.99))
+            HStack {
+                Text("9th grade")
+                    .multilineTextAlignment(.leading)
+                
+                Text("14 years old")
+                    .multilineTextAlignment(.trailing)
+                
+            }
             Image("catpicture")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-          
-            
-            Text("Charlotte Hwang")
-            HStack {
-                Text("9th grade")
-                Text("14 years old")
-            }
-            
+                .cornerRadius(20)
+            Text("This is me and my kitten, Junie!")
         }
         .padding()
     }
