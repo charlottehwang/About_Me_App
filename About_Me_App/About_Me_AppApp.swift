76aeaf94b8263d4167a6a7ab770ac240b77a0b08
Charlotte <charlotteehwang@gmail.com>
2025-07-23 12:54:38 -0700

    Initial Commit

---
+++ About_Me_App/About_Me_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_Me_AppApp.swift
+//  About_Me_App
+//
+//  Created by Charlotte Hwang on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct About_Me_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
